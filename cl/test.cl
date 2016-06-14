0b7ae04edb52923009b338007e52f9a1ee755f24
xy0 <xiz368@eng.ucsd.edu>
2016-06-14 16:27:45 -0700

    settingup

--- cl/test.cl
+++ cl/test.cl
@@ -1,3 +1,5 @@
-kernel void my_kernel( int N, global float *one, global float *two, local float *one_local, global float *result ) {
-    // kernel code here...
+kernel void test (global float *in, global float *out) {
+    const int global_id = get_global_id(0);
+    out[global_id] = in[global_id] + 7;
 }
+
--- playground.cpp
+++ playground.cpp
@@ -1,8 +1,33 @@
 #include "easycl/EasyCL.h"
-
+#include "util.h"
 using namespace std;
 
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    if( !EasyCL::isOpenCLAvailable() ) {
+        cout << "opencl library not found" << endl;
+        exit(-1);
+    }
+    cout << "found opencl library" << endl;
+    EasyCL *cl = EasyCL::createForFirstGpu();
+    CLKernel *kernel = cl->buildKernelFromString(getKernelRelative("cl/test.cl"), "test", "");
+
+    CLArrayFloat *out = cl->arrayFloat(5);
+    float in[5];
+    for(int i = 0; i < 5; i++) {
+        in[i] = i * 3;
+    }
+    kernel->in(5, in)->out(out)->run_1d(5, 5);
+    assertEquals((*out)[0] , 7);
+    assertEquals((*out)[1] , 10);
+    assertEquals((*out)[2] , 13);
+    assertEquals((*out)[3] , 16);
+    assertEquals((*out)[4] , 19);
+    cout << "tests completed ok" << endl;
+
+    delete out;
+    delete kernel;
+    delete cl;
     return 0;
 }
